1b17f116f1e8c33cce9b910d83e221745ebb33f8
alinegfchagas <alinegfchagas@hotmail.com>
2022-05-10 15:25:07 -0300

    operadores

---
+++ modulo1/operadores/index.js
@@ -0,0 +1,80 @@
+/*
+Exercício interpretação 1 
+
+const bool1 = true
+const bool2 = false
+const bool3 = !bool2
+
+let resultado = bool1 && bool2
+console.log("a. ", resultado)                  // false
+
+resultado = bool1 && bool2 && bool3 
+console.log("b. ", resultado)                // false
+
+resultado = !resultado && (bool1 || bool2)  //false
+console.log("c. ", resultado)
+
+console.log("d. ", typeof resultado)*/    // Boolean
+
+
+/*exercício interpretação 2 
+
+
+--o comando vai apenas concatenar os resultados, visto que estão no modo string. --
+
+exercício interpretação 3 
+
+o código executável para realizar o comando seria: 
+
+
+let primeiroNumero = prompt("Digite um numero!")
+let segundoNumero = prompt("Digite outro numero!")
+
+const soma = Number(primeiroNumero) + Number(segundoNumero)
+
+console.log(soma)
+
+
+
+exercício de digitação 1
+
+const idadeUsuario = Number (prompt("informe sua idade"))
+const melhorAmigo = Number (prompt("informe a idade do seu/a melhor amigo/a"))
+
+console.log ("Sua idade é maior do que a do seu melhor amigo?", idadeUsuario > melhorAmigo,"diferença de idade", idadeUsuario-melhorAmigo)
+
+
+exercício digitação 2
+
+const numeroPar = Number (prompt ("informe um número par"))
+const resultado = numeroPar % 2
+console.log (resultado)
+
+exercício digitação 3 
+
+const idade = Number (prompt("informe sua idade"))
+const meses = 12
+const dias = idade * 365
+const horas = dias * 24
+
+console.log (idade * 12)
+console.log (dias)
+console.log (horas)
+
+
+exercício 4 digitação
+
+const num1 = Number (prompt ("escreva um numero"))
+const num2 = Number (prompt ("escreva outro numero"))
+
+console.log ("O primeiro numero é maior que segundo?", num1 > num2)
+console.log ("O primeiro numero é igual ao segundo?",num1 ===num2) 
+console.log ("O primeiro numero é divisível pelo segundo?", num1 % num2 == 0) 
+console.log ("O segundo numero é divisível pelo primeiro?", num2 % num1 == 0) */
+  
+
+
+
+
+
+
