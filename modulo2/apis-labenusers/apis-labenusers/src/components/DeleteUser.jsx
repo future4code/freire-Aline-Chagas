b0242c8ca2c074f2cc4c74f60d257be406bae669
alinegfchagas <alinegfchagas@hotmail.com>
2022-06-21 17:45:17 -0300

    função deletar

--- modulo2/apis-labenusers/apis-labenusers/src/components/DeleteUser.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default class DeleteUser extends React.Component {
-    
-}
--- modulo2/apis-labenusers/apis-labenusers/src/components/ListaUser.jsx
+++ modulo2/apis-labenusers/apis-labenusers/src/components/ListaUser.jsx
@@ -6,7 +6,7 @@ export default class ListaUser extends React.Component {
   state = {
     listaUsuario: []
   }
-  
+
   componentDidMount() {
     this.getAllUsers()
   }
@@ -25,11 +25,28 @@ export default class ListaUser extends React.Component {
     });
 
   }
+  deleteUsers = (id) => {
+    axios.delete(
+      `https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users/${id}`,
+     
+      {
+        headers: {
+          Authorization: "aline-chagas-freire"
+        }
+      }
+    ).then((response) => {
+      alert("Usuário excluído")
+      this.getAllUsers()
+    });
+
+  }
+
 
   render() {
     const listaUser = this.state.listaUsuario.map(usuario => {
       return (<div key={usuario.id}>
         <p>{usuario.name}</p>
+        <button onClick={()=> this.deleteUsers(usuario.id)}>deletar</button>
       </div>)
     })
     return (
