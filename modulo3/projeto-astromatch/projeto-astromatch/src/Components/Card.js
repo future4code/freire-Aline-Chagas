e920941ba571a2b77d6a90e8c6f129cc7e3d862f
alinegfchagas <alinegfchagas@hotmail.com>
2022-07-07 20:11:45 -0300

    funcionalidades implementadas e estilização parcial

--- modulo3/projeto-astromatch/projeto-astromatch/src/Components/Card.js
+++ modulo3/projeto-astromatch/projeto-astromatch/src/Components/Card.js
@@ -3,37 +3,125 @@ import { useState } from "react";
 import { useEffect } from "react";
 import axios from "axios";
 import styled from "styled-components";
+import like from "../img/like.png";
+import unlike from "../img/unlike.png";
 
 const Estilizacao = styled.div`
   border: 1px solid black;
-  width: 300px;
+  width: 400px;
   height: 600px;
   align-content: center;
   margin: auto 0;
+  background-color: white;
+  border-radius: 10px;
 
-  img {
-    width: 100%;
+ #imagem{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+    width: 400px;
+    height: 360px;
     border-radius: 5px;
+    overflow: hidden;
   }
-  p {
-    text-align: center;
+
+  #imagem::after{
+    content: "";
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    background: url(${props => props.imagem});
+    background-size: cover;
+    filter: blur(30px);
+    z-index: 2;
   }
+
+
+  #imagem::before{
+    content: "";
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    right: 1px;
+    background: linear-gradient(0deg,rgb(0 0 0) 10%,rgba(0,0,0,0) 73%);
+    background-size: cover;
+    filter: blur(30px);
+    z-index: 6;
+  }
+
+  #perfil{
+    width: 100%;
+    z-index: 5;
+  }
+
+  p{
+    font-size: 25px;
+  }
+
+  h2 {
+    text-align: start;
+    font-size: large;
+  }
+
+  h1{
+    text-align: left;
+  }
+
   button {
-    justify-content: space-evenly;
+    border-radius: 50%;
+    border: none;
+    background: none;
+    
+  }
+
+  .botoes {
+    width: 80px;
+    height: 80px;
+    cursor: pointer;
+  }
+
+  #estilizacaoPerfil {
+    position: relative;
+    overflow: hidden;
+    width: 95%;
+    margin: 0 auto;
+    border-radius: 30px;
+    box-shadow: 0px 0px 7px 1px #000000;
+  }
+
+  #bioPerfil{
+    position: absolute;
+    left: 10px;
+    bottom: 25px;
+    color: white;
+    width: fit-content;
+    z-index: 50;
+
+  }
+
+  #botaoMatch{
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    height: 130px;
   }
 `;
 
-export default function Card() {
+export default function Card(props) {
   const [perfil, setPerfil] = useState({});
   const [escolherPerfil, setEscolherPerfil] = useState();
+  const [telas, setTelas] = useState(false);
 
   const PerfilExibido = () => {
     axios
       .get(
-        "https://us-central1-missao-newton.cloudfunctions.net/astroMatch/aline-chagas-freire/person"
+        "https://us-central1-missao-newton.cloudfunctions.net/astroMatch/aline-freire/person"
       )
       .then((response) => {
-        setPerfil(response.data.profile);
+        // if(response.data.profile === null){
+        //   return ({MostraPerfil})
+        // }
+        setPerfil(response.data.profile || {end: true});
       })
 
       .catch((err) => {
@@ -43,49 +131,68 @@ export default function Card() {
   const EscolhePerfil = () => {
     axios
       .post(
-        "https://us-central1-missao-newton.cloudfunctions.net/astroMatch/aline-chagas-freire/choose-person",
+        "https://us-central1-missao-newton.cloudfunctions.net/astroMatch/aline-freire/choose-person",
         {
-          id:perfil.id,
+          id: perfil.id,
           choice: escolherPerfil,
-        })
+        }
+      )
 
       .then((response) => {
-      PerfilExibido()
+        PerfilExibido();
       })
       .catch((err) => {
         console.log(err.response);
       });
   };
 
-  const Like = () =>{
+  const Like = () => {
     EscolhePerfil();
     setEscolherPerfil(true);
-  }; 
+  };
 
-  const Dislike= () =>{
+  const Dislike = () => {
     EscolhePerfil();
     setEscolherPerfil(false);
-  }; 
+  };
 
   const MostraPerfil = () => {
     return (
-      <Estilizacao>
+      <Estilizacao imagem={perfil.photo}>
         <p>Astromatch</p>
-        <p>
-          {perfil.name}, {perfil.age}
-        </p>
-        <img src={perfil.photo} alt={perfil.name} />
-        <p>{perfil.bio}</p>
-        <button onClick={Dislike}> x </button>
-        <button onClick={Like}> ♥ </button>
-      </Estilizacao>
-    );
+        <button onClick={()=>props.setTelas("teste")}>Matches</button>
+        <button onClick={props.LimpaLista}>Limpar Matches</button>
+
+        <div id="estilizacaoPerfil">
+
+          <div id="imagem">
+
+          <img id="perfil" src={perfil.photo} alt={perfil.name} />
+          </div>
+
+          <div id="bioPerfil">
+            <h1>
+              {perfil.name}, {perfil.age}
+            </h1>
+            <h2>{perfil.bio}</h2>
+          </div>
+        </div>
+
+        <div id="botaoMatch">
+        <button onClick={Dislike}>
+          <img className="botoes" src={unlike} />
+        </button>
+        <button onClick={Like}>
+          <img className="botoes" src={like} />
+        </button>
+        </div>
+
+      </Estilizacao>)
   };
 
   useEffect(() => {
     PerfilExibido();
   }, []);
 
-  return <MostraPerfil />;
+  return <MostraPerfil />
 }
-// cards perfis 
--- modulo3/projeto-astromatch/projeto-astromatch/src/Components/Matches.js
+++ modulo3/projeto-astromatch/projeto-astromatch/src/Components/Matches.js
@@ -4,20 +4,34 @@ import { useEffect } from "react";
 import axios from "axios";
 import styled from "styled-components";
 
+const ContainerLista = styled.div`
+border: 1px solid black;
+  width: 400px;
+  height: 600px;
+  align-content: center;
+  margin: auto 0; 
+  background-color: white;
+  border-radius: 10px;
+`
+
 const ListaMatches = styled.div `
 display: flex;
 flex-direction: row;
+
 img {
-  width: 15px;
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+
 }
 `
 
-export default function Matches() {
+export default function Matches(props) {
     const [match, setMatch] = useState([]);
 
     const ListaMatch = () => {
         axios.get(
-          "https://us-central1-missao-newton.cloudfunctions.net/astroMatch/aline-chagas-freire/matches")
+          "https://us-central1-missao-newton.cloudfunctions.net/astroMatch/aline-freire/matches")
           .then((response) => {
             setMatch(response.data.matches);
           })
@@ -26,22 +40,23 @@ export default function Matches() {
           });
       };
 
-console.log(match);
 
 useEffect(() => {
     ListaMatch();
   }, []);
 
 return (
-    <div>
+    <ContainerLista>
+        <p>Astromatch</p>
+        <button onClick={()=>props.setTelas("inicio")}>Inicio</button>
+        <button onClick={props.LimpaLista}>Limpar Matches</button>
      {match.map((match)=>{ 
     return (
-      <ListaMatches>
+      <ListaMatches key={match.id}>
         <img src={match.photo} alt = ""/>
         <p>{match.name}</p>
         </ListaMatches>
     )
      })}
-    </div>)
+    </ContainerLista>)
 }    
-// componente matches
