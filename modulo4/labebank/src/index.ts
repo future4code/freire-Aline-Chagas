8d8c074c72dcc8b83cf684e6b52b69a523830d97
alinegfchagas <alinegfchagas@hotmail.com>
2022-08-20 13:20:05 -0300

    a muuito custo, endpoint cria user

--- modulo4/labebank/src/index.ts
+++ modulo4/labebank/src/index.ts
@@ -1,21 +1,34 @@
 import express from "express";
 import cors from "cors";
-import { user } from "./user"
+import { user } from "./user";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-const bankList = user
+const bankList = user;
 // endpoint lista usuario
-app.get("/userList", (req, res) => {
-    res.send(bankList);
-  });
+// app.get("/userList", (req, res) => {
+//     res.send(bankList);
+//   });
 
 // endpoint criar conta
+app.post("/addAccount", (req, res) => {
+  const newAccount = user;
+  const { id, name, age, cpf, birth } = req.body;
+  const addAccount = { id, name, age, cpf, birth };
 
+  if (age < 18) {
+    throw new Error("Desculpe! menores de 18 não podem criar conta :(");
+  }
+  newAccount.push(addAccount);
+
+  res.send(addAccount);
+});
+
+// endpoint 
 
 app.listen(3003, () => {
-    console.log("Server is running in http://localhost:3003")
+  console.log("Server is running in http://localhost:3003");
 });
--- modulo4/labebank/src/user.ts
+++ modulo4/labebank/src/user.ts
@@ -18,15 +18,12 @@ export const user: User[]=[
         age: 33,
         cpf: 55528245808,
         birth:'01/01/1989'
-        
-
     },
     {   id: 2,
         name: 'Clara',
         age: 30,
         cpf: 17528865809,
         birth:'10/07/1992'
-
     },
     {   id: 3,
         name: 'José',
@@ -40,6 +37,5 @@ export const user: User[]=[
         age: 29,
         cpf: 22527845810,
         birth:'01/01/1993'
-
     }
 ]
