d5262f7f01a27f73437d0c03a8a4101f69389e5d
Aline Gonçalves Fontes Chagas <alinegfchagas@hotmail.com>
2022-09-14 17:55:55 -0300

    inicio projeto

---
+++ modulo4/projeto-cookenu/src/index.ts
@@ -0,0 +1,10 @@
+import app from "./app";
+
+
+app.post("/user/signup", );
+app.put("/user/edit", );
+app.post("/user/login", );
+app.get("/user/profile", )
+
+
+
---
+++ modulo4/projeto-cookenu/src/types.ts
@@ -0,0 +1,18 @@
+export type user = {
+   id: string
+   email: string
+   password: string
+   name: string
+   nickname: string
+   role:userRole
+}
+
+export type AuthenticationData ={
+   id: string
+   role:userRole
+}
+
+export enum userRole {
+   NORMAL = "NORMAL",
+   ADMIN = "ADMIN"
+}
