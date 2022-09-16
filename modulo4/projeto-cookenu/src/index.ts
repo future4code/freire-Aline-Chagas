6132532cde88fe65463ea41c4bfc4397c6119b60
Aline Gonçalves Fontes Chagas <alinegfchagas@hotmail.com>
2022-09-15 21:36:43 -0300

    endpoint logiin

--- modulo4/projeto-cookenu/src/index.ts
+++ modulo4/projeto-cookenu/src/index.ts
@@ -1,9 +1,10 @@
 import app from "./app";
+import login from "./endpoints/login";
 import signUp from "./endpoints/signUp"
 
 
 app.post("/users/signup",signUp );
-app.post("/users/login", );
+app.post("/users/login", login );
 app.get("/users/profile", )
 app.get("/users/:id/profile", )
 
